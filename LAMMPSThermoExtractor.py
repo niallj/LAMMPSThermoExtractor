8bbc0d003449ddd1accd8e3a1c8c163cf94f7f72
Niall Jackson <niall.jackson@gmail.com>
2015-11-30 11:58:17 +0000

    Added timing information extractor, this is now printed to the end of the files.

--- LAMMPSThermoExtractor.py
+++ LAMMPSThermoExtractor.py
@@ -1,5 +1,5 @@
 class LAMMPSThermoData:
-  def __init__(self, thermolines):
+  def __init__(self, thermolines, timing_data=None):
     import numpy as np
 
     self.names = thermolines.pop(0).split()
@@ -8,10 +8,24 @@ class LAMMPSThermoData:
     cols = len(self.names)
     self.values = self.values.reshape((rows, cols))
 
+    if timing_data:
+      self.time, self.procs, self.steps, self.atoms = list(map(float, timing_data))
+
+  def timing_string(self):
+    if self.time:
+      output = "{steps} steps in {time} seconds.\n".format(
+          steps=int(self.steps), time=self.time)
+      output += "{s_per_step:e} seconds per step (~{steps_per_day:.2e} steps per day)\n".format(
+          s_per_step=self.time/self.steps, steps_per_day=self.steps/self.time*86400)
+      return output
+    else:
+      return "No timing data available."
+
 class LAMMPSLogFile:
   #these are the start and end points of the thermodynamic data block in the log
   thermo_block_begin = "Memory usage per processor"
   thermo_block_end = "Loop time"
+  timing_regex = "Loop time of ([0-9.]+) on ([0-9]+) procs \(.+\) for ([0-9]+) steps with ([0-9]+) atoms"
 
   thermodata_blocks = []
 
@@ -19,6 +33,7 @@ class LAMMPSLogFile:
     self.filename = filename
 
   def parse(self):
+    import re #needed to extract timing information
     def fragment_check(frag,line):
       return (line[0:len(frag)] == frag)
 
@@ -29,8 +44,15 @@ class LAMMPSLogFile:
         if inside_thermo_block:
           if fragment_check(self.thermo_block_end, line):
             inside_thermo_block = False
-            ###TODO: Parse timing information
-            self.thermodata_blocks.append(LAMMPSThermoData(current_thermo_block))
+            regex = re.compile(self.timing_regex)
+            m = regex.match(line)
+            if not m:
+              print("Error! Couldn't perform the regex match on the loop time information. The offending line was:")
+              print(line)
+              self.thermodata_blocks.append(LAMMPSThermoData(current_thermo_block))
+            else:
+              time, procs, steps, atoms = m.group(1, 2, 3, 4)
+              self.thermodata_blocks.append(LAMMPSThermoData(current_thermo_block, (time, procs, steps, atoms)))
             current_thermo_block.clear()
           else:
             current_thermo_block.append(line)
@@ -40,7 +62,7 @@ class LAMMPSLogFile:
 
   def loop_blocks(self):
     for b in self.thermodata_blocks:
-      yield b.names, b.values
+      yield b
 
 if __name__ == "__main__":
   import argparse
@@ -57,17 +79,19 @@ if __name__ == "__main__":
   l.parse()
 
   for i,b in enumerate(l.loop_blocks()):
-    n = b[0]
+    n = b.names
     filename = args.filepattern.format(i=i)
     if args.onlyfields:
       columns = [i for i,name in enumerate(n) if name in args.onlyfields]
       if len(columns) == 0:
         continue
       else:
-        v = b[1][:, columns]
+        v = b.values[:, columns]
         headerline = " ".join(args.onlyfields)
-        np.savetxt(filename, v, header=headerline)
+        #include the timing data at the end, but not the final newline
+        np.savetxt(filename, v, header=headerline, footer=b.timing_string()[:-1])
     else:
-      v = b[1]
+      v = b.values
       headerline = " ".join(n)
-      np.savetxt(filename, v, header=headerline)
+      #include the timing data at the end, but not the final newline
+      np.savetxt(filename, v, header=headerline, footer=b.timing_string()[:-1])
